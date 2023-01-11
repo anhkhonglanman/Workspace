452de764559b6840da2f3b01690ad2e5e3201869
anhkhonglanman <quyen283hn@gmail.com>
2023-01-11 15:20:49 +0700

    new file

---
+++ JS/luyen_tap_JS/index.js
@@ -0,0 +1,20 @@
+
+// console.log([...(new Set)])
+
+// function deQuy(num) {
+//     if (num < 0) {
+//         //stop
+//         return;
+//     } 
+//     deQuy();
+// }
+// deQuy(10);
+
+
+let text = "";
+let i = 0 ;
+while (i <= 10) {
+    text += "the number is :" + i + "<br>";
+    i++
+}
+document.write(text)
